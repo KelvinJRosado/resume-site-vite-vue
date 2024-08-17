186865794ec222b1ef85b4fc2c25dae2e21ec877
Kelvin Rosado <kelvin.rosado.ayala@gmail.com>
2024-08-17 11:58:33 -0400

    Update color

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,8 @@ import PageHeader from './components/PageHeader.vue';
 }
 
 .active {
-  background-color: #ff7400;
-  border-color: #ff7400;
+  background-color: #0f78a2;
+  border-color: #0f78a2;
   border-radius: 4px;
   color: white;
   pointer-events: none;
