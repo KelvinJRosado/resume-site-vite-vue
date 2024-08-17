ad7ada414fb70cc1a9b2e7f7d638b68f632e02a9
Kelvin Rosado <kelvin.rosado.ayala@gmail.com>
2024-08-17 00:50:00 -0400

    Update rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,30 +4,32 @@ import pluginVue from 'eslint-plugin-vue';
 import tseslint from 'typescript-eslint';
 
 export default [
-    eslint.configs.all,
-    ...tseslint.configs.strict,
-    ...pluginVue.configs['flat/recommended'],
-    eslintConfigPrettier,
-    { ignores: ['dist/'] },
-    {
-        files: ['src/**/*.ts', 'src/**/*.vue', '*.ts', '*.vue', '*.js'],
-        languageOptions: {
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                sourceType: 'module',
-            },
-        },
-        rules: {
-            'capitalized-comments': 'off',
-            'no-console': 'off',
-            'no-magic-numbers': 'off',
-            'no-plusplus': 'off',
-            'no-useless-assignment': 'off',
-            'no-var': 'warn',
-            'prefer-const': 'warn',
-            'sort-imports': 'off',
-            'vue/attribute-hyphenation': 'off',
-            'vue/order-in-components': 'off',
-        },
+  eslint.configs.all,
+  ...tseslint.configs.strict,
+  ...pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
+  { ignores: ['dist/'] },
+  {
+    files: ['src/**/*.ts', 'src/**/*.vue', '*.ts', '*.vue', '*.js'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+      },
     },
+    rules: {
+      'capitalized-comments': 'off',
+      'no-console': 'off',
+      'no-magic-numbers': 'off',
+      'no-plusplus': 'off',
+      'no-useless-assignment': 'off',
+      'no-var': 'warn',
+      'one-var': 'off',
+      'prefer-const': 'warn',
+      'sort-imports': 'off',
+      'sort-keys': 'off',
+      'vue/attribute-hyphenation': 'off',
+      'vue/order-in-components': 'off',
+    },
+  },
 ];
