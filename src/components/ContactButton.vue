d5399c37e77e288ccd7ca02ac318b03952410884
Kelvin Rosado <kelvin.rosado.ayala@gmail.com>
2024-08-16 23:57:24 -0400

    Added contact button component

---
+++ src/components/ContactButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{ displayText: string; link: string }>();
+</script>
+
+<template>
+    <button class="contact-button">
+        <a :href="link" target="_blank">{{ displayText }}</a>
+    </button>
+</template>
+
+<style scoped>
+button {
+    font-size: 24px;
+    margin-bottom: 10px;
+    border: 1px solid #333;
+    padding: 5px;
+
+    margin-bottom: 10px;
+}
+
+a {
+    color: #333;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.config.errorHandler = (err) => {
+    /* handle error */
+    console.error((err as Error).message);
+};
+
+app.mount('#app');
