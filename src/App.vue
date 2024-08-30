e0fc0c8d49f825436be2a4701f64e72f66636fda
Kelvin Rosado <kelvin.rosado.ayala@gmail.com>
2024-08-29 20:57:53 -0400

    Fixed styling

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,29 @@
 import PageHeader from './components/PageHeader.vue';
 import { routes } from './main';
 import Toolbar from 'primevue/toolbar';
-import { PrimeIcons } from '@primevue/core/api';
 import { RouterLink } from 'vue-router';
 import Button from 'primevue/button';
 </script>
 
 <template>
   <PageHeader></PageHeader>
-  <Toolbar>
-    <template v-for="route in routes" #start>
-      <nav>
+  <Toolbar style="border-radius: 3rem; padding: 1rem 1rem 1rem 1.5rem">
+    <template #start>
+      <div class="flex items-center gap-2">
         <Button
-          :as="RouterLink"
+          v-for="route in routes"
           :key="route.name"
+          :as="RouterLink"
           :label="route.name"
-          :icon="PrimeIcons.CAR"
           :to="route.path"
           :class="'nav-button'"
           :active-class="'active'"
+          :icon="route.icon"
+          text
+          raised
+          outlined
         ></Button>
-      </nav>
+      </div>
     </template>
   </Toolbar>
   <main>
@@ -30,15 +33,6 @@ import Button from 'primevue/button';
 </template>
 
 <style scoped>
-.nav-button {
-  display: inline-block;
-  padding: 8px 16px;
-  border: 1px solid #ccc;
-  background-color: #f0f0f0;
-  text-decoration: none;
-  color: #333;
-}
-
 .active {
   background-color: #0f78a2;
   border-color: #0f78a2;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import EducationPage from './components/Pages/EducationPage.vue';
 import ExperiencePage from './components/Pages/ExperiencePage.vue';
@@ -8,22 +8,30 @@ import SkillsPage from './components/Pages/SkillsPage.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css';
+import { PrimeIcons } from '@primevue/core/api';
 
 const basePath = '/resume-site-vite-vue';
 
-export const routes: RouteRecordRaw[] = [
-  { path: '/', component: HomePage, name: 'Home' },
+export const routes = [
+  { path: '/', component: HomePage, name: 'Home', icon: PrimeIcons.HOME },
   {
     path: '/education',
     component: EducationPage,
     name: 'Education',
+    icon: PrimeIcons.STAR,
   },
   {
     path: '/experience',
     component: ExperiencePage,
     name: 'Experience',
+    icon: PrimeIcons.BRIEFCASE,
   },
-  { path: '/skills', component: SkillsPage, name: 'Skills' },
+  {
+    path: '/skills',
+    component: SkillsPage,
+    name: 'Skills',
+    icon: PrimeIcons.USERS,
+  },
 ];
 
 const router = createRouter({
